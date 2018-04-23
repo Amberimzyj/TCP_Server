d1d38268fa36dc0473972e23f23c6cb17032b59b
Amberimzyj <amberimzyj@outlook.com>
2018-04-23 17:15:52 +0800

    Receive data successfully

--- TCP Server.py
+++ TCP Server.py
@@ -20,13 +20,14 @@ print('Waiting for connection...')
 
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
-    sock.send(b'Welcome!')
+    # sock.send(b'Welcome!')
     while True:
         data = sock.recv(1024)
         time.sleep(1)
-        if not data or data.decode('utf-8') == 'exit':
+        if not data or data.decode('GB2312') == 'exit':
             break
-        sock.send(('Hello,%s!' % data.decode('utf-8')).encode('utf-8'))
+        print(data.decode('GB2312'))
+        # sock.send(('Hello,%s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
 
@@ -36,4 +37,5 @@ while True:
     sock, addr = s.accept()
     # 创建新线程来处理TCP连接
     t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.setDaemon(True)
     t.start()

15437cbda024a781595fa305c025dcb9436112de
Amberimzyj <amberimzyj@outlook.com>
2018-05-06 19:58:35 +0800

    Initial commit

--- TCP_Client.py
+++ TCP_Client.py
@@ -3,7 +3,7 @@
 # @Emial:  amberimzyj@outlook.com
 # @Date:   2018-04-23 13:10:31
 # @Last Modified by:   Amberimzyj
-# @Last Modified time: 2018-04-23 14:18:28
+# @Last Modified time: 2018-04-26 21:06:23
 # @License: MIT LICENSE
 # 创建一个客户端接收服务器数据
 import socket
@@ -14,9 +14,9 @@ ip = socket.gethostbyname(socket.gethostname())
 s.connect((ip, 8088))
 # 接收欢迎消息
 print(s.recv(1024).decode('utf-8'))
-for data in [b'Amber', b'Susan', b'Shlll']:
+for data in [b'Amber\r\n', b'Susan\r\n', b'Shlll\r\n']:
     # 发送数据
     s.send(data)
-    print(s.recv(1024).decode('utf-8'))
-s.send(b'exit')
+    # print(s.recv(1024).decode('utf-8'))
+# s.send(b'exit\r\n')
 s.close()
--- TCP_Server.py
+++ TCP_Server.py
@@ -17,17 +17,26 @@ s.bind(('0.0.0.0', 8088))
 s.listen(5)
 print('Waiting for connection...')
 
+def waitconnect():
+    # 创建一个socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 监听端口
+    s.bind(('0.0.0.0', 8088))
+    s.listen(5)
+    print('Waiting for connection...')
+
 
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
-    # sock.send(b'Welcome!')
+    sock.send(b'Welcome!')
     while True:
         data = sock.recv(1024)
         time.sleep(1)
-        if not data or data.decode('GB2312') == 'exit':
+        if not data or data.decode('utf-8') == 'exit':
             break
-        print(data.decode('GB2312'))
-        # sock.send(('Hello,%s!' % data.decode('utf-8')).encode('utf-8'))
+        print(data.decode('utf-8'))
+        sock.send(('Hello,%s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
 
